7167d25b2618317eb69b7c707f85e2b67d9b3696
Alexis Boisserand <APB14@acuitysso.com>
2023-12-04 21:59:15 -0500

    Removes one character.

--- day4a/src/main.rs
+++ day4a/src/main.rs
@@ -1,16 +1,16 @@
-use std::collections::HashSet;
+    use std::collections::HashSet;
 
-fn main() {
-    let sum: u32 = include_str!("input.txt")
-        .lines()
-        .filter_map(|line| {
-            let (winning, mine) = line.split_once(':').unwrap().1.split_once('|').unwrap();
-            let winning: HashSet<_> = winning.split_whitespace().collect();
-            let matches = mine.split_whitespace()
-                .filter_map(|n| winning.contains(&n).then_some(()))
-                .count() as u32;
-            (matches > 0).then(|| 2u32.pow(matches - 1))
-        })
-        .sum();
-    println!("{sum}");
-}
+    fn main() {
+        let sum: u32 = include_str!("input.txt")
+            .lines()
+            .filter_map(|line| {
+                let (winning, mine) = line.split_once(':').unwrap().1.split_once('|').unwrap();
+                let winning: HashSet<_> = winning.split_whitespace().collect();
+                let matches = mine.split_whitespace()
+                    .filter_map(|n| winning.contains(n).then_some(()))
+                    .count() as u32;
+                (matches > 0).then(|| 2u32.pow(matches - 1))
+            })
+            .sum();
+        println!("{sum}");
+    }

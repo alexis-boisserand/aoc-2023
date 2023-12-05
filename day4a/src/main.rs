fe15e7ecd5d34fc415fbd023dc9edaca9c29acc9
Alexis Boisserand <APB14@acuitysso.com>
2023-12-04 21:56:18 -0500

    No need to convert to number.

--- day4a/src/main.rs
+++ day4a/src/main.rs
@@ -1,17 +1,12 @@
 use std::collections::HashSet;
 
-fn to_numbers(s: &str) -> impl Iterator<Item = u32> + '_ {
-    s.split(|ch: char| !ch.is_ascii_digit())
-        .filter_map(|s| s.parse::<u32>().ok())
-}
-
 fn main() {
     let sum: u32 = include_str!("input.txt")
         .lines()
         .filter_map(|line| {
             let (winning, mine) = line.split_once(':').unwrap().1.split_once('|').unwrap();
-            let winning: HashSet<_> = HashSet::from_iter(to_numbers(winning));
-            let matches = to_numbers(mine)
+            let winning: HashSet<_> = winning.split_whitespace().collect();
+            let matches = mine.split_whitespace()
                 .filter_map(|n| winning.contains(&n).then_some(()))
                 .count() as u32;
             (matches > 0).then(|| 2u32.pow(matches - 1))

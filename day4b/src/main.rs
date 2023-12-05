b594dcdae5698f7820c130fd2bc0c70b57a89b29
Alexis Boisserand <APB14@acuitysso.com>
2023-12-04 21:40:50 -0500

    Small improvement.

--- day4b/src/main.rs
+++ day4b/src/main.rs
@@ -1,30 +1,21 @@
 use std::collections::HashSet;
 
-fn to_numbers(s: &str) -> impl Iterator<Item = u32> + '_ {
-    s.split(|ch: char| !ch.is_ascii_digit())
-        .filter_map(|s| s.parse::<u32>().ok())
-}
-
 fn main() {
     let matches: Vec<_> = include_str!("input.txt")
         .lines()
         .map(|line| {
             let (winning, mine) = line.split_once(':').unwrap().1.split_once('|').unwrap();
-            let winning: HashSet<_> = HashSet::from_iter(to_numbers(winning));
-            to_numbers(mine)
+            let winning: HashSet<_> = winning.split_whitespace().collect();
+            mine.split_whitespace()
                 .filter_map(|n| winning.contains(&n).then_some(()))
                 .count() as u32
         })
         .collect();
 
     let mut instances = vec![1u32; matches.len()];
-    for i in 0..instances.len() {
-        if matches[i] > 0 {
-            for j in i + 1..=i + matches[i] as usize {
-                instances[j] += instances[i];
-            }
-        }
-    }
+    (0..instances.len()).for_each(|i| {
+        (i + 1..=i + matches[i] as usize).for_each(|j| instances[j] += instances[i]);
+    });
 
     let sum = instances.iter().sum::<u32>();
     println!("{sum}");
